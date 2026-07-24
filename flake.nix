268567e694f05f65f16dbdf090b9a8c28931edf8
Sumner Evans <me@sumnerevans.com>
2026-07-24 13:02:03 -0600

    flake: init
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nevarro LLC financials";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              hledger
+              python3
+            ];
+          };
+        };
+    };
+}
